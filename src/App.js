48c428952c79e5475ec28973ec6364a9f238357e
TIKOsup <temirlan.kystaubayev@gmail.com>
2023-11-28 23:06:59 +0600

    feat: add section with first 10 pokemons as default
    feat: add button to show more pokemons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 
+const api_base_url = "https://pokeapi.co/api/v2/";
+const limit = 10;
+
 function Pokemon({ id }) {
   const [pokemonData, setPokemonData] = useState([]);
   const [pokemonImg, setPokemonImg] = useState({});
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + id)
+    fetch(`${api_base_url}pokemon/${id}`)
     .then(res => res.json())
     .then(data => {
       setPokemonData(data);
@@ -28,16 +31,40 @@ function Pokemon({ id }) {
   );
 }
 
-export default function Profile() {
+export default function Pokedex() {
+  const [offset, setOffset] = useState(0);
+  const [pokemonPage, setPokemonPage] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${api_base_url}pokemon?${offset}` + (typeof limit !== "underfined" && `&limit=${limit}`))
+    .then(res => res.json())
+    .then(data => {
+      setPokemonPage(data);
+      setPokemonList(data.results);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+  }, []);
+
+  function handleMore() {
+    // TODO
+    setOffset(offset + limit);
+  }
+
   return (
     <>
       <header>
         <h1>Pokedex</h1>
       </header>
       <main>
-        <Pokemon id={1} />
-        <Pokemon id={2} />
-        <Pokemon id={3} />
+        <section>
+          {pokemonList.map(pokemon => {
+            return <Pokemon id={pokemon.name} />
+          })}
+        </section>
+        <button onClick={handleMore}>Show More</button>
       </main>
     </>
   );
